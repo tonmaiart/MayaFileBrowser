4d86a87d42e4494df254539e397a5ad8062b504e
Natchapon Srisuk <natchapon.18851@gmail.com>
2026-08-11 11:57:45 +0700

    register to maya_launcher

---
+++ maya-scripts/UkoreBrowser/__init__.py
@@ -0,0 +1,16 @@
+import maya.cmds as cmds
+
+try:
+    from UkoreMenu import registry, MenuItemSpec
+
+    registry.register_item(
+        MenuItemSpec(
+            id="maya_file_browser",
+            label="Maya File Browser...",
+            category="จัดการไฟล์",
+            command="from tmlib.core import File; File.launch('UkoreBrowser')",
+            order=10,
+        )
+    )
+except ImportError:
+    pass
